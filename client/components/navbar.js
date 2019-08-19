d542da8beeea603cb51beec700cbbecc4de0e8ec
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-19 17:08:09 -0400

    login component created backend needs to be added in order to work completely

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,26 +3,28 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import AuthForm from './auth-form'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>Music Mapper</h1>
     <nav>
-      {isLoggedIn ? (
-        <div>
+      Visualize Your Musical Tastes!
+      {/* {isLoggedIn ? (
+        <div> */}
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          {/* <Link to="/home">Home</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
         </div>
       ) : (
-        <div>
+        <div> */}
           {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
+          {/* <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
-      )}
+      )} */}
     </nav>
     <hr />
   </div>
