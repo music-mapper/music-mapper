71bf1146de10051c47d696ff3771bf51e6739382
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-20 12:40:49 -0400

    removed history

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 // import {auth} from '../store'
-import history from '../history'
+
 
 /**
  * COMPONENT
