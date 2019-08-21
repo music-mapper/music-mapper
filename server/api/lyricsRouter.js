3bdd80c7ef9b9efdd69c3d258d72bcccbd2461e3
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-21 11:36:25 -0400

    Wrap Lyrics api request in a GET route

--- server/api/lyricsRouter.js
+++ server/api/lyricsRouter.js
@@ -4,35 +4,19 @@ require('../../secrets')
 let querystring = require('querystring')
 module.exports = router
 
-// this is assuming that "access_token" is available as a global variable!!
-// global.access_token
+  router.get('/', function (req, res) {
+    var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Despacito%20 (Featuring%20Daddy%20Yankee)&q_artist=Luis%20Fonsi&apikey=${process.env.api_key}`,
+    json: true
+    }
 
-// router.get('/', function(request, res) {
-  // console.log(`Access token is ${global.access_token}`)
-  // let data
-  // var options = {
-  //   url: 'https://api.spotify.com/v1/me/following?type=artist',
-  //   headers: {Authorization: 'Bearer ' + global.access_token},
-  //   json: true
-  // }
-  // // use the access token to access the Spotify Web API
-  // request.get(options, function(error, response, body) {
-  //   data = body
-  //   console.log('ARTISTS GENRE: ', body.artists.items[0].genres)
-
-  request(
-    // `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Thriller&q_artist=Michael%20Jackson&apikey=${
-    //   process.env.api_key
-    // }`,
-    `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Despacito%20 (Featuring%20Daddy%20Yankee)&q_artist=Luis%20Fonsi&apikey=${
-      process.env.api_key
-    }`,
-    function(error, response, body) {
+    request.get(options, function(error, response, body) {
       console.log('error:', error) // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
       console.log('body:', body) // Print the HTML for the Google homepage.
-    }
-  )
+    })
 
-  // res.redirect('localhost:8888')
-// })
+    res.redirect('localhost:8888')
+  })
+
+
+
