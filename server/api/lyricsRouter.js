fe2ad975dcc248d94630a2b7bf2eed8ac544e200
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-21 12:06:10 -0400

    Setup lyricsRouter to get artist and trackname Variables

--- server/api/lyricsRouter.js
+++ server/api/lyricsRouter.js
@@ -4,10 +4,27 @@ require('../../secrets')
 let querystring = require('querystring')
 module.exports = router
 
+  // request(
+  //   // `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Thriller&q_artist=Michael%20Jackson&apikey=${
+  //   //   process.env.api_key
+  //   // }`,
+  //   `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Despacito%20 (Featuring%20Daddy%20Yankee)&q_artist=Luis%20Fonsi&apikey=${
+  //     process.env.api_key
+  //   }`
+  // )
+
+  //artist name - Luis Fonsi
+  //track name -  Despacito (Featuring Daddy Yankee)
+
+  let q_artist = "";
+  let q_track = "";
+
   router.get('/', function (req, res) {
-    var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Despacito%20 (Featuring%20Daddy%20Yankee)&q_artist=Luis%20Fonsi&apikey=${process.env.api_key}`,
+    var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Despacito%20(Featuring%20Daddy%20Yankee)&q_artist=Luis%20Fonsi&apikey=${process.env.api_key}`,
     json: true
     }
+
+    // var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=${Despacito%20(Featuring%20Daddy%20Yankee)}&q_artist=${Luis%20Fonsi}&apikey=${process.env.api_key}`, json: true}
 
     request.get(options, function(error, response, body) {
       console.log('error:', error) // Print the error if one occurred
