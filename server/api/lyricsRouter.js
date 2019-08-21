f90d1bb8b57713f17b1483096e769a38535c733c
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-21 14:48:42 -0400

    verify using tracks in lyrics api route without spaces

--- server/api/lyricsRouter.js
+++ server/api/lyricsRouter.js
@@ -16,15 +16,29 @@ module.exports = router
   //artist name - Luis Fonsi
   //track name -  Despacito (Featuring Daddy Yankee)
 
-  let q_artist = "";
-  let q_track = "";
+
 
   router.get('/', function (req, res) {
-    var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Despacito%20(Featuring%20Daddy%20Yankee)&q_artist=Luis%20Fonsi&apikey=${process.env.api_key}`,
-    json: true
-    }
+    let q_artist = res.artist
+    // let q_artist = "Luis Fonsi"
 
-    // var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=${Despacito%20(Featuring%20Daddy%20Yankee)}&q_artist=${Luis%20Fonsi}&apikey=${process.env.api_key}`, json: true}
+    let q_track = res.name
+    // let q_track = "Despacito (Featuring Daddy Yankee)"
+
+    // var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=Despacito%20(Featuring%20Daddy%20Yankee)&q_artist=Luis%20Fonsi&apikey=${process.env.api_key}`,
+    // json: true
+    // }
+
+  //artist name - Luis Fonsi
+  // Luis%20Fonsi
+
+  //track name -  Despacito (Featuring Daddy Yankee)
+  // Despacito%20(Featuring%20Daddy%20Yankee)
+
+
+     var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=${q_track}&q_artist=${q_artist}&apikey=${process.env.api_key}`,
+     json: true}
+
 
     request.get(options, function(error, response, body) {
       console.log('error:', error) // Print the error if one occurred
