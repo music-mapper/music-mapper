170e660318c1c7b10e5f99395f82c4eafdd93ef4
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-22 17:54:49 -0400

    it works finally

--- server/api/tracksRouter.js
+++ server/api/tracksRouter.js
@@ -2,61 +2,90 @@ const router = require('express').Router()
 let request = require('request')
 // let querystring = require('querystring')
 module.exports = router
+var rp = require('request-promise')
+
 
 // this is assuming that "access_token" is available as a global variable!!
 // global.access_token
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   var trackInfo = []
-  try {
-    let wordCounter = {}
+  let concatArr = []
+  let wordCounter = {}
+
   console.log(`Access token is ${global.access_token}`)
   var options = {
     url: 'https://api.spotify.com/v1/me/tracks',
-    headers: {Authorization: 'Bearer ' + global.access_token},
+    headers: {
+      Authorization: 'Bearer ' + global.access_token,
+      'User-Agent': 'Request-Promise'
+    },
     json: true
   }
   // use the access token to access the Spotify Web API
-  request.get(options, function(error, response, body) {
-    body.items.map(song =>
-    trackInfo.push({artist: song.track.artists[0].name,
-    id: song.track.id,
-    name: song.track.name})
-    )
-    trackInfo.forEach(function(track){
-      let q_track= track.name
-      let q_artist = track.artist
-      // console.log(q_artist)
-      // console.log(q_track)
-
-      var lyricsOptions = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${q_track}&q_artist=${q_artist}&apikey=${process.env.api_key}`,
-      json: true}
-
-      request.get(lyricsOptions, async function(error, response, body) {
-        console.log('hi')
-        const rawData = await body.message.body.lyrics.lyrics_body.toUpperCase()
-        const secondReg = /[.,\/#!?$%\^&\*;:{}=\-_~()"\d\n]/g
-        let newStr = rawData.replace(secondReg, ' ')
-         //slice from -7 at end to remove needless warning
-        let newArray = newStr.split(' ').filter(el => el !== '').slice(0, -7)
-        // count the words--could do more than one here
-        newArray.forEach(word => {
-          wordCounter[word] = (wordCounter[word]) ? wordCounter[word] + 1 : 1
+  try {
+  const response = await rp(options)
+      response.items.map(song =>
+        trackInfo.push({
+          artist: song.track.artists[0].name,
+          id: song.track.id,
+          name: song.track.name
         })
-        console.log(wordCounter)
-    })
-  })
-  let outputData = []
-  for (key in wordCounter) {
-   outputData.push({
-     Name: key,
-     Count: wordCounter[key]
-   })
+      )
+        for (let i = 0; i < trackInfo.length; i++){
+          const q_artist =  trackInfo[i].artist
+          const q_track = trackInfo[i].name
+          var lyricsOptions = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${q_track}&q_artist=${q_artist}&apikey=${process.env.api_key}`,
+          json: true}
+          const lyrics = await rp(lyricsOptions)
+          const rawData = lyrics.message.body.lyrics.lyrics_body.toUpperCase()
+          const secondReg = /[.,\/#!?$%\^&\*;:{}=\-_~()"\d\n]/g
+          let newStr = rawData.replace(secondReg, ' ')
+          //slice from -7 at end to remove needless warning
+          let newArray = newStr.split(' ').filter(el => el !== '').slice(0, -7)
+          concatArr.push(...newArray)
+
+        }
+        concatArr.forEach(word => {
+          wordCounter[word] = wordCounter[word] ? wordCounter[word] + 1 : 1
+        })
+        let outputData = []
+          for (key in wordCounter) {
+           outputData.push({
+             Name: key,
+             Count: wordCounter[key]
+           })
+          }
+         res.json(outputData)
+
   }
- res.json(outputData)
+
+
+    catch(error) {
+      console.log('failed', error)
+    }
+
 })
-  }
-catch (err) {
-  next(err)
-}
-})
+
+
+
+
+//     })
+//   })
+//   let outputData = []
+//   for (key in wordCounter) {
+//     console.log('output function')
+//    outputData.push({
+//      Name: key,
+//      Count: wordCounter[key]
+//    })
+//   }
+//  res.json(outputData)
+// }).catch(function (error){
+//   console.log('failed')
+// })
+//   }
+// catch (err) {
+//   next(err)
+// }
+// })
