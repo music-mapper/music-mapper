8836efe6b7a8b57a19ec178bb37fcf3585ac95d9
CMorgan-Feir <morganfeir@gmail.com>
2019-08-22 12:12:52 -0400

    attempt at finalizing lyrics route, needs tweaking

--- server/api/tracksRouter.js
+++ server/api/tracksRouter.js
@@ -7,56 +7,55 @@ module.exports = router
 // global.access_token
 
 router.get('/', function(req, res, next) {
+  var trackInfo = []
   try {
-  // console.log(`Access token is ${global.access_token}`)
-  // let data
-  // var options = {
-  //   url: 'https://api.spotify.com/v1/me/tracks',
-  //   headers: {Authorization: 'Bearer ' + global.access_token},
-  //   json: true
-  // }
-  // // use the access token to access the Spotify Web API
-  // request.get(options, function(error, response, body) {
+    let wordCounter = {}
+  console.log(`Access token is ${global.access_token}`)
+  var options = {
+    url: 'https://api.spotify.com/v1/me/tracks',
+    headers: {Authorization: 'Bearer ' + global.access_token},
+    json: true
+  }
+  // use the access token to access the Spotify Web API
+  request.get(options, function(error, response, body) {
+    body.items.map(song =>
+    trackInfo.push({artist: song.track.artists[0].name,
+    id: song.track.id,
+    name: song.track.name})
+    )
+    trackInfo.forEach(function(track){
+      let q_track= track.name
+      let q_artist = track.artist
+      // console.log(q_artist)
+      // console.log(q_track)
 
-  //   const  trackInfo = []
-  //   body.items.map(song =>
-  //   trackInfo.push({artist: song.track.artists[0].name,
-  //   id: song.track.id,
-  //   name: song.track.name})
-  //   )
+      var lyricsOptions = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${q_track}&q_artist=${q_artist}&apikey=${process.env.api_key}`,
+      json: true}
 
-  //   // res.send(trackInfo)
-  // })
-
-
-  q_track= 'man in the mirror'
-  q_artist = 'michael jackson'
-
-  var options = {url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${q_track}&q_artist=${q_artist}&apikey=${process.env.api_key}`,
-  json: true}
-
-  // json: true
- request.get(options, async function(error, response, body) {
-   const rawData = await body.message.body.lyrics.lyrics_body.toUpperCase()
-   const secondReg = /[.,\/#!?$%\^&\*;:{}=\-_~()"\d\n]/g
-   let newStr = rawData.replace(secondReg, ' ')
-    //slice from -7 at end to remove needless warning
-   newArray = newStr.split(' ').filter(el => el !== '').slice(0, -7)
-   // count the words--could do more than one here
-   let wordCounter = {}
-   newArray.forEach(word => {
-     wordCounter[word] = (wordCounter[word]) ? wordCounter[word] + 1 : 1
-   })
-   let outputData = []
-   for (key in wordCounter) {
-    outputData.push({
-      Name: key,
-      Count: wordCounter[key]
+      request.get(lyricsOptions, async function(error, response, body) {
+        console.log('hi')
+        const rawData = await body.message.body.lyrics.lyrics_body.toUpperCase()
+        const secondReg = /[.,\/#!?$%\^&\*;:{}=\-_~()"\d\n]/g
+        let newStr = rawData.replace(secondReg, ' ')
+         //slice from -7 at end to remove needless warning
+        let newArray = newStr.split(' ').filter(el => el !== '').slice(0, -7)
+        // count the words--could do more than one here
+        newArray.forEach(word => {
+          wordCounter[word] = (wordCounter[word]) ? wordCounter[word] + 1 : 1
+        })
+        console.log(wordCounter)
     })
-   }
-  res.json(outputData)
- })
-}
+  })
+  let outputData = []
+  for (key in wordCounter) {
+   outputData.push({
+     Name: key,
+     Count: wordCounter[key]
+   })
+  }
+ res.json(outputData)
+})
+  }
 catch (err) {
   next(err)
 }
