b8919d4df7068b3bb1f3032bd8794efa66a32d86
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-23 15:20:52 -0400

    started working on BarChart it renders dummy data will work on backend to get data back how we need it also started working on gauge chart but nothing works there yet

--- client/components/react-bubble-chart.js
+++ client/components/react-bubble-chart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BubbleChart from './bubble-chart'
 import StreamGraph from './streamGraph';
+import BarGraph from './firstBarChart'
 
 export default class ReactBubbleChart extends React.Component{
   render(){
@@ -8,6 +9,7 @@ export default class ReactBubbleChart extends React.Component{
       <div>
         <BubbleChart size={[500,500]} />
         <StreamGraph />
+        <BarGraph />
       </div>
     )
   }
