28102e91652c064b8b4f3f6922ef5e0ff931a787
CMorgan-Feir <morganfeir@gmail.com>
2019-08-24 18:16:45 -0400

    refactored code

--- client/routes.js
+++ client/routes.js
@@ -1,22 +1,15 @@
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Link} from 'react-router-dom'
-import PropTypes from 'prop-types'
+import {Route, Switch} from 'react-router-dom'
 import {Login} from './components'
-// import {me} from './store'
 import ReactBubbleChart from './components/react-bubble-chart'
-import BarGraph from './components/firstBarChart';
+import BarGraph from './components/firstBarChart'
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
-    // this.props.loadInitialData()
-  }
 
   render() {
-    const {isLoggedIn} = this.props
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
@@ -30,33 +23,4 @@ class Routes extends Component {
   }
 }
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    // isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    // loadInitialData() {
-    //   dispatch(me())
-    // }
-  }
-}
-
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
-
-/**
- * PROP TYPES
- */
-Routes.propTypes = {
-  // loadInitialData: PropTypes.func.isRequired,
-  // isLoggedIn: PropTypes.bool.isRequired
-}
+export default Routes
