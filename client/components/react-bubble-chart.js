5a89c6ef135d4767179f353bbbf67f97b9669b54
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-24 19:10:35 -0400

    Trying to get Collision Chart to render

--- client/components/react-bubble-chart.js
+++ client/components/react-bubble-chart.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import BubbleChart from './bubble-chart'
-import CollisionChart from './collision-chart'
+import CollisionChart from './collisionHTML'
 
 
 export default class ReactBubbleChart extends React.Component{
   render(){
     return <div>
         <BubbleChart size={[1000, 1000]} />
-        <CollisionChart size={[1000, 1000]} />
+        <CollisionChart/>
       </div>
   }
 }
