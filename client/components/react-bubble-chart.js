6038834a172091f02817b0015314c4c215544879
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-24 12:29:59 -0400

    Create Collision-chart component

--- client/components/react-bubble-chart.js
+++ client/components/react-bubble-chart.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import BubbleChart from './bubble-chart'
+import CollisionChart from './collision-chart'
 
 
 export default class ReactBubbleChart extends React.Component{
   render(){
-    return(
-      <div>
-        <BubbleChart size={[1000,1000]} />
+    return <div>
+        <BubbleChart size={[1000, 1000]} />
+        <CollisionChart size={[1000, 1000]} />
       </div>
-    )
   }
 }
