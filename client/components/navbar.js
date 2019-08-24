a1745bb2240044dbc8799e3b642bd27786ee6751
CMorgan-Feir <morganfeir@gmail.com>
2019-08-24 08:30:19 -0400

    removed user store and thinned out navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,58 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
-import AuthForm from './auth-form'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = () => (
   <div>
     <h1>Music Mapper</h1>
     <nav>
       Visualize Your Musical Tastes!
-      {/* {isLoggedIn ? (
-        <div> */}
-          {/* The navbar will show these links after you log in */}
-          {/* <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div> */}
-          {/* The navbar will show these links before you log in */}
-          {/* <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )} */}
     </nav>
     <hr />
   </div>
 )
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    // isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    // handleClick() {
-    //   dispatch(logout())
-    // }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Navbar)
-
-/**
- * PROP TYPES
- */
-Navbar.propTypes = {
-  // handleClick: PropTypes.func.isRequired,
-  // isLoggedIn: PropTypes.bool.isRequired
-}
+export default Navbar
