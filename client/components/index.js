0128d5898917f4975ef70799ed25e6bf63e69d97
CMorgan-Feir <morganfeir@gmail.com>
2019-08-25 18:34:22 -0400

    working version for new splash/login page

--- client/components/index.js
+++ client/components/index.js
@@ -3,5 +3,4 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './navbar'
-export {default as Login } from './auth-form'
+export {default as Login } from './auth-form' // could we make the naming more consistent?
