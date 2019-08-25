5f588c27e22810b07d6d7abfac0ebf5e755fb3d4
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-25 00:46:24 -0400

    working on backend routes for streamgraph and rendering simple streamGraph in the front end with react

--- server/api/artistRouter.js
+++ server/api/artistRouter.js
@@ -2,33 +2,24 @@ const router = require('express').Router()
 let request = require('request')
 // let querystring = require('querystring')
 module.exports = router
-const rp = require('request-promise')
 
 // this is assuming that "access_token" is available as a global variable!!
 // global.access_token
 
-router.get('/', async function(req, res) {
-  let trackInfo = []
+router.get('/', function(req, res) {
   console.log(`Access token is ${global.access_token}`)
+  let data
   var options = {
     url: 'https://api.spotify.com/v1/me/following?type=artist',
-    headers: {
-      Authorization: 'Bearer ' + global.access_token,
-      'User-Agent': 'Request-Promise'
-    },
+    headers: {Authorization: 'Bearer ' + global.access_token},
     json: true
   }
   // use the access token to access the Spotify Web API
-  try {
-    const response = await rp(options)
-      response.artists.items.map(artist =>{
-        trackInfo.push({
-          genre: artist.genres,
-          artist: artist.name
-        })
-      })
-    res.json(trackInfo)
-  } catch (error) {
-    console.log('It didn\'t work', error)
-  }
+  request.get(options, function(error, response, body) {
+    data = body
+    // console.log('ARTISTS GENRE: ', body.artists.items[0].genres)
+    console.log('ARTISTS NAME: ', body.artists.items[0].name)
+  })
+
+  res.redirect('localhost:8888')
 })
