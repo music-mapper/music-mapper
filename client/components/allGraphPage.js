ce143d50e126a389b8cbee4190680a0a6a1649eb
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-27 12:36:27 -0400

    Add comments to allGraphPage

--- client/components/allGraphPage.js
+++ client/components/allGraphPage.js
@@ -13,14 +13,13 @@ export default class ReactBubbleChart extends React.Component {
   }
   render() {
     console.log(this.state)
+    //everytime the state changes, it removed the old graph so we can load the new one
     d3.selectAll('svg > *').remove()
     return (
       <div className="app">
         <div className="app-container">
-
-
           <div className="app-navigation">
-
+            {/* anchor buttons inside navigation bar */}
             <a onClick={() => this.setState({currentChart: 'CollisionChart'})}className="navigation-link">
               <span className="nav-icon">
                 <i className="fa fa-file-text-o" aria-hidden="true" />
@@ -57,16 +56,10 @@ export default class ReactBubbleChart extends React.Component {
               </span>
               <span className="nav-label">Artists</span>
             </a>
-
-
           </div>
 
           <div className="app-content">
-
-          <div id= 'logout-button-container'>
-          <h1 id='logout-button'> LOGOUT </h1>
-          </div>
-
+            {/* only renders graph when state is equal to 'GenreMap' */}
             {this.state.currentChart === 'GenreMap' && <GenreMap />}
 
             {this.state.currentChart === 'Triangle' && <Triangle />}
