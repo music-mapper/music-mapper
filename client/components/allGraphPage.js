48be7106e4ac17b80cd3f3ae0fc1154547332328
nataliegarate <natalieegarate@hotmail.com>
2019-08-27 16:36:49 -0400

    added logout functionality

--- client/components/allGraphPage.js
+++ client/components/allGraphPage.js
@@ -9,73 +9,85 @@ import * as d3 from 'd3'
 export default class AllGraphPage extends React.Component {
   constructor() {
     super()
-    this.state = {currentChart: 'CollisionChart'}
+    this.state = { currentChart: 'CollisionChart' }
   }
+
+  async logout() {
+    const url = 'https://www.spotify.com/logout/'
+    const spotifyLogoutWindow = window.open(url, 'Spotify Logout', 'width=700,height=500,top=40,left=40')
+    await setTimeout(() => spotifyLogoutWindow.close(), 2000)
+    window.location.replace("http://localhost:8888");
+  }
+
   render() {
     console.log(this.state)
 
     //everytime the state changes, it removed the old graph so we can load the new one
     d3.selectAll('svg > *').remove()
-    return <div className="app">
-        <div className="app-container">
-          <div className="app-navigation">
-            {/* anchor buttons inside navigation bar */}
-            <a onClick={() => this.setState({currentChart: 'CollisionChart'})}className="navigation-link">
-              <span className="nav-icon">
-                <i className="fa fa-file-text-o" aria-hidden="true" />
-              </span>
-              <span className="nav-label">Lyrics</span>
-            </a>
 
-            <a onClick={() => this.setState({
-                  currentChart: 'Triangle'
-                })} className="navigation-link">
-              <span className="nav-icon">
-                <i class="fa fa-headphones" aria-hidden="true" />
-              </span>
-              <span className="nav-label">Features</span>
-            </a>
+    return (<div className="app">
+      <div className="app-container">
+        <div className="app-navigation">
+          {/* anchor buttons inside navigation bar */}
 
-            <a onClick={() => this.setState({
-                  currentChart: 'GenreMap'
-                })} className="navigation-link">
-              <span className="nav-icon">
-                <i className="fa fa-music" aria-hidden="true" />
-              </span>
-              <span className="nav-label">Genres</span>
-            </a>
+          <a onClick={() => this.setState({ currentChart: 'CollisionChart' })} className="navigation-link">
+            <span className="nav-icon">
+              <i className="fa fa-file-text-o" aria-hidden="true" />
+            </span>
+            <span className="nav-label">Lyrics</span>
+          </a>
+
+          <a onClick={() => this.setState({ currentChart: 'Triangle' })} className="navigation-link">
+            <span className="nav-icon">
+              <i className="fa fa-headphones" aria-hidden="true" />
+            </span>
+            <span className="nav-label">Features</span>
+          </a>
+
+          <a onClick={() => this.setState({ currentChart: 'GenreMap' })} className="navigation-link">
+            <span className="nav-icon">
+              <i className="fa fa-music" aria-hidden="true" />
+            </span>
+            <span className="nav-label">Genres</span>
+          </a>
 
 
 
-            <a onClick={() => this.setState({
-                  currentChart: 'BarChart'
-                })} className="navigation-link">
-              <span className="nav-icon">
-                <i className="fa fa-star" aria-hidden="true" />
-              </span>
-              <span className="nav-label">Artists</span>
-            </a>
+          <a onClick={() => this.setState({
+            currentChart: 'BarChart'
+          })} className="navigation-link">
+            <span className="nav-icon">
+              <i className="fa fa-star" aria-hidden="true" />
+            </span>
+            <span className="nav-label">Artists</span>
+          </a>
 
 
-            <a href='https://accounts.spotify.com/en/logout' className="navigation-link">
-              <span className="nav-icon">
-              <i class="fas fa-sign-out-alt"></i>              </span>
-              <span className="nav-label">Logout</span>
-            </a>
 
-          </div>
-          <div className="app-content">
+          <a onClick={() => this.logout()} className="navigation-link">
+            <span className="nav-icon">
+              <i className="fa fa-sign-out-alt"></i>
+            </span>
+            <span className="nav-label">Logout</span>
+          </a>
 
-            {/* only renders graph when state is equal to 'GenreMap' */}
-            {this.state.currentChart === 'GenreMap' && <GenreMap />}
+        </div>
 
-            {this.state.currentChart === 'Triangle' && <Triangle />}
+        <div className="app-content">
 
-            {this.state.currentChart === 'BarChart' && <BarChart />}
+          {/* only renders graph when state is equal to 'GenreMap' */}
 
-            {this.state.currentChart === 'CollisionChart' && <CollisionChart />}
-          </div>
+          {this.state.currentChart === 'GenreMap' && <GenreMap />}
+
+          {this.state.currentChart === 'Triangle' && <Triangle />}
+
+          {this.state.currentChart === 'BarChart' && <BarChart />}
+
+          {this.state.currentChart === 'CollisionChart' && <CollisionChart />}
+
         </div>
       </div>
+    </div>
+    )
   }
 }
