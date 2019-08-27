3c539c8bfe95950704093d659deeab72931676b4
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-27 17:13:44 -0400

    some changes

--- client/components/viewData.js
+++ client/components/viewData.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {gotAllFeatures, gotArtistsFreq, gotAllLyrics, gotAllGenres} from './store'
+import {gotAllFeatures, gotArtistsFreq, gotAllLyrics, gotAllGenres} from '../store'
 
 class ViewData extends React.Component{
-  componentDidMount(){
+  async componentDidMount(){
+    try {
+      await this.props.gotAllFeatures()
+      await this.props.gotAllGenres()
+      await this.props.gotAllLyrics()
+      await this.props.gotArtistsFreq()
+
+    } catch (error) {
+      console.log('error in the viewData componenet', error)
+    }
+
 
   }
 
   render(){
+    let {frequency, genres, features} = this.props
+    console.log(frequency)
+    console.log('genres: ', genres)
+    console.log('features: ', features)
     return(
-      <div>
-
+      <div> Hello
+        <p> {this.props.features.data}</p>
       </div>
     )
   }
