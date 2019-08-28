95f4f9447b12f4c8ec5a1e5391a0696def6285e4
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-28 16:44:06 -0400

    added error page

---
+++ client/components/error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ErrorPage() {
+  return(
+    <div id='error-page'>
+      <h2> 404s and Heartbreak</h2>
+      <img id ='error-img' src='https://morganfeir.s3.us-east-2.amazonaws.com/images/404-error.png' />
+      <p>Unescaped characters in your song titles mean we can't show this graph right now. Please check again soon!</p>
+    </div>
+  )
+}
