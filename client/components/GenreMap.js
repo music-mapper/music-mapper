1c79a3dc5ab4402c0e4e445bf54cec35fcbf082b
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-28 12:00:54 -0400

    Add Loading component to Genre Graph

--- client/components/GenreMap.js
+++ client/components/GenreMap.js
@@ -2,6 +2,7 @@ import React from 'react';
 import WordCloud from 'react-d3-cloud';
 import {gotAllGenres} from '../store/genres';
 import {connect} from 'react-redux'
+import Loading from './Loading'
 
 const fontSizeMapper =  word => Math.log2(word.value) * 15;
 const rotate = word => word.value % 360;
@@ -15,8 +16,8 @@ class GenreMap extends React.Component{
     }
   }
 render(){
-  if (this.props.genres.data === undefined){
-    return []
+  if (this.props.genres.length === 0){
+    return (<Loading />)
   }
 
   return(<WordCloud
