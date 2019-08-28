a192368c7b17e9e775c648dc99a2787022000944
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-28 16:56:24 -0400

    Redirect URI to Localhost for Friends-and-Family event

--- server/auth/spotify.js
+++ server/auth/spotify.js
@@ -4,7 +4,8 @@ let querystring = require('querystring')
 module.exports = router
 
 let redirect_uri =
-  process.env.REDIRECT_URI || 'https://spotify-music-mapper.herokuapp.com'
+  process.env.REDIRECT_URI || 'http://localhost:8888/auth/spotify/callback'
+  // process.env.REDIRECT_URI || 'https://spotify-music-mapper.herokuapp.com'
 let access_token = ''
 
 
@@ -41,7 +42,8 @@ router.get('/callback', function(req, res) {
     // we want to send this access token back to store somewhere...
     // (TODO how will we store this for later use?)
     global.access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'https://spotify-music-mapper.herokuapp.com'
+    let uri = process.env.FRONTEND_URI || 'http://localhost:8888/app'
+    // let uri = process.env.FRONTEND_URI || 'https://spotify-music-mapper.herokuapp.com/app'
     res.redirect(uri) // could redirect you to "logged in"
   })
 })
