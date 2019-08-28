0008f13201d30abecfcf035760f5d8961eadc63a
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-28 12:58:03 -0400

    Removed Bootstrap from Loading Component

--- client/components/Loading.js
+++ client/components/Loading.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function loading() {
   return <div id='loading-box'>
-      <img src="https://media1.tenor.com/images/db85ba00c6073b451a8f05156a66524e/tenor.gif?itemid=9856796" width="800"/>
+      <img src="https://media1.tenor.com/images/db85ba00c6073b451a8f05156a66524e/tenor.gif?itemid=9856796" />
     </div>
 }
