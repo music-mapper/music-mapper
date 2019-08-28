5c516b69f890feb55bcf8bf2402e637f0d5dd82c
nataliegarate <natalieegarate@hotmail.com>
2019-08-28 17:18:50 -0400

    final changes

--- client/components/error.js
+++ client/components/error.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function ErrorPage() {
   return(
     <div id='error-page'>
+      <br/> <br/> <br/>
       <h2> 404s and Heartbreak</h2>
       <img id ='error-img' src='https://morganfeir.s3.us-east-2.amazonaws.com/images/404-error.png' />
       <p>Unescaped characters in your song titles mean we can't show this graph right now. Please check again soon!</p>
