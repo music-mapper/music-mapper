a2fb17a6b27e508c6c5282d062521a2210f64adf
Tae Hui Chang <jasmine878@yahoo.com>
2019-08-29 15:28:05 -0400

    Remove all references to LocalHost

--- server/auth/spotify.js
+++ server/auth/spotify.js
@@ -5,7 +5,6 @@ module.exports = router
 const session = require('express-session')
 
 let redirect_uri =
-  // process.env.REDIRECT_URI || 'http://localhost:8888/auth/spotify/callback'
   process.env.REDIRECT_URI || 'https://spotify-music-mapper.herokuapp.com'
 
 
@@ -42,7 +41,6 @@ router.get('/callback', function(req, res) {
     // we want to send this access token back to store somewhere...
     // (TODO how will we store this for later use?)
     req.session.access_token = body.access_token
-    // let uri = process.env.FRONTEND_URI || 'http://localhost:8888/app'
     let uri = process.env.FRONTEND_URI || 'https://spotify-music-mapper.herokuapp.com/app'
 
 
